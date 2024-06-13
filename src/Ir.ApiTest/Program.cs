92cd1d2ad1919e0a5a01558392ea482526a72896
Jonathan Taylor <jhhtaylor@gmail.com>
2024-06-13 13:22:08 +0100

    Initial commit 2

---
+++ src/Ir.ApiTest/Program.cs
@@ -0,0 +1,30 @@
+using Ir.IntegrationTest.Entity;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+  options.SerializerSettings.Converters.Add(new StringEnumConverter { NamingStrategy = new CamelCaseNamingStrategy() });
+  options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(options => options.UseInMemoryDatabase(databaseName: "Database"));
+
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
